0c14b86b417b0397022af38a50bcc803bb0eb3b4
brunobonaldi94 <brunobonaldi94@gmail.com>
2022-04-13 17:55:51 -0300

    added check lstadd_front

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: bbonaldi <bbonaldi@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 19:14:17 by bbonaldi          #+#    #+#             */
-/*   Updated: 2022/04/11 20:32:06 by bbonaldi         ###   ########.fr       */
+/*   Updated: 2022/04/13 17:55:31 by bbonaldi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
+	if (!lst || !new)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
 	new->next = (*lst);
 	(*lst) = new;
 }
